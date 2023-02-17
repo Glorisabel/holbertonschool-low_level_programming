edb81e07dc0d324b680d843f88ff6c05754fc881
Glorisabel <glorisabelriv@gmail.com>
2023-02-17 17:06:43 +0000

    first job c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - entry point
- * Return: program should end with the value 0
- */
+* main - entry point
+* * Return: program should end with the value 0
+*/
   int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
