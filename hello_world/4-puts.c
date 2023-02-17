375c3fe0a7ac7e1cac252ecc8afd6f7a7d03449f
Glorisabel <glorisabelriv@gmail.com>
2023-02-17 17:01:52 +0000

    first work in C

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,10 @@
-#!/bin/bash
 #include <stdio.h>
-  int main ()
-	puts() ; "Programming is like building a multilingual puzzle";
-	return 0;
+/**
+ * main - entry point
+ * Return: program should end with the value 0
+ */
+  int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}	
